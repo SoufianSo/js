a937f08ed0d5b6d28f204df23d96a98183c42113
Soufian <soufian.souilm@gmail.com>
2023-10-16 21:03:03 +0200

    js

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+//intro-level-1-3
+console.log("number");
+console.log("string");
+console.log("string");
+console.log("boolean");
+console.log("string");
+console.log("Live reload enabled");
+//intro-level-1-4
+
+console.log(typeof "John");
+console.log(typeof 10);
+console.log(typeof NaN);
+console.log(typeof false);
+console.log(typeof [1, 2, 3, 4]);
+console.log(typeof { name: "John", age: 34 });
+console.log(typeof new Date());
+console.log(typeof function () {});
+console.log(typeof null);
+console.log(typeof (3 + 2 == 5));
+console.log(typeof (3 + "3"));
+//intro-level-1-6
+let x = 34;
+x = x + 31;
+x += 1;
+x++;
+console.log(x);
+let y = 34;
+y = y + 31;
+y += 1;
+y++;
+console.log(y);
+
+//intro-level-1-6
+// Variablen deklarieren
+// Variablen deklarieren
+const f = 20;
+const g = 30;
+
+console.log("f + g =", f + g);
+
+console.log("f - g =", f - g);
+
+console.log("g - f =", g - f);
+
+console.log("f * g =", f * g);
+
+console.log("f / g =", f / g);
+
+// Neue Variable h deklarieren
+const h = 10;
+
+const resultOne = (f * g) / h;
+
+// Weitere Variablen deklarieren
+let a = 15;
+let b = 9;
+
+// Rest beim Teilen von a durch b
+const remainder = a % b;
+console.log("Rest von a / b =", remainder);
+
+// Neue Variable c deklarieren
+const c = 20;
